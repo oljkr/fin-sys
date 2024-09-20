416489276b615e30b77679b7df9042a6c04898b5
oljkr <zaoljkr@gmail.com>
2024-09-20 16:17:23 +0900

    first commit

---
+++ src/main/java/kr/wishtarot/finsys/FinSysApplication.java
@@ -0,0 +1,13 @@
+package kr.wishtarot.finsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/wishtarot/finsys/FinSysApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.wishtarot.finsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
